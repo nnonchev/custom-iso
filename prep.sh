04abf70b56203ca10e6fffd5ab6d8133407e3566
Nikola Nonchev <nnonchev@protonmail.com>
2020-04-24 21:01:15 +0200

    Added chmod to prep script

--- prep.sh
+++ prep.sh
@@ -2,3 +2,5 @@
 
 echo "Preapring for build..."
 cp -r ./misc ./pre-install.sh ./post-install.sh ./releng/airootfs/root/
+chmod +x ./releng/airootfs/root/pre-install.sh
+chmod +x ./releng/airootfs/root/pre-install.sh
